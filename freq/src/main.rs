99f5155dc036be5c4af6fe659208f036a1effaca
Jesse A. Tov <jesse.tov@gmail.com>
2016-01-07 12:24:23 -0600

    end of class

--- freq/src/main.rs
+++ freq/src/main.rs
@@ -1,3 +1,43 @@
+use std::collections::HashMap;
+
+#[doc="
+Counts the frequencies of words read from the standard input, and print
+a sorted frequency table.
+
+Assumptions:
+"]
 fn main() {
-    println!("Hello, world!");
+    // let word_counts = read_and_count(...);
+    // print_counts(word_counts);
+}
+
+// bool
+// char
+// usize (like size_t)
+// ssize (like ssize_t)
+// u8, u16, u32, u64
+// s8, ...
+// f32, f64
+//
+// String (like std::string)
+// &str (like const char*)
+
+// impl<K, V, S> HashMap<K, V, S> {
+//     fn get(&self, k: &K) -> Option<&V> {
+//         ...
+//     }
+// }
+
+// let π = e;
+// stms...
+//
+//   ===
+//
+// match e {
+//   π => stms...
+// }
+
+fn increment_word(mut map: &mut HashMap<String, usize>, word: String) {
+    let count_ref = map.entry(word).or_insert(0);
+    *count_ref += 1;
 }
