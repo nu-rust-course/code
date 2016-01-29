0423e7dc4be0f90e09c760aa503517ee3ee923f4
Jesse A. Tov <jesse.tov@gmail.com>
2016-01-29 11:13:34 -0600

    After 1/28 class

--- ownership/src/bst.rs
+++ ownership/src/bst.rs
@@ -2,6 +2,15 @@ use std::cmp::Ordering::*;
 
 pub struct BST<K, V>(NodePtr<K, V>);
 
+struct Node<K, V> {
+    key:   K,
+    value: V,
+    left:  NodePtr<K, V>,
+    right: NodePtr<K, V>,
+}
+
+type NodePtr<K, V> = Option<Box<Node<K, V>>>;
+
 impl<K, V> BST<K, V> {
     pub fn new() -> Self {
         BST(None)
@@ -14,55 +23,35 @@ impl<K, V> BST<K, V> {
 
 impl<K: Ord, V> BST<K, V> {
     pub fn find(&self, key: &K) -> Option<&V> {
-        Node::find_iter(&self.0, key)
+        Node::find(&self.0, key)
     }
 
     pub fn find_mut(&mut self, key: &K) -> Option<&mut V> {
-        Node::find_mut_rec(&mut self.0, key)
+        Node::find_mut(&mut self.0, key)
     }
 }
 
-struct Node<K, V> {
-    key:   K,
-    value: V,
-    left:  NodePtr<K, V>,
-    right: NodePtr<K, V>
-}
-type NodePtr<K, V> = Option<Box<Node<K, V>>>;
-
 impl<K, V> Node<K, V> {
     fn len(ptr: &NodePtr<K, V>) -> usize {
-        if let &Some(ref b) = ptr {
-            1 +Node::len(&b.left) + Node::len(&b.right)
+        if let &Some(ref n) = ptr {
+            1 + Node::len(&n.left) + Node::len(&n.right)
         } else {0}
     }
 }
 
 impl<K: Ord, V> Node<K, V> {
-    pub fn find_rec<'a>(ptr: &'a NodePtr<K, V>, key: &K) -> Option<&'a V> {
-        if let Some(ref n) = *ptr {
+    fn find<'a, 'b>(ptr: &'a NodePtr<K, V>, key: &'b K) -> Option<&'a V> {
+        if let &Some(ref n) = ptr {
             match key.cmp(&n.key) {
-                Less    => Node::find_rec(&n.left, key),
-                Greater => Node::find_rec(&n.right, key),
+                Less    => Node::find(&n.left, key),
+                Greater => Node::find(&n.right, key),
                 Equal   => Some(&n.value),
             }
         } else {None}
     }
 
-    fn find_mut_rec<'a>(ptr: &'a mut NodePtr<K, V>, key: &K)
-        -> Option<&'a mut V>
-    {
-        if let Some(ref mut n) = *ptr {
-            match key.cmp(&n.key) {
-                Less    => Node::find_mut_rec(&mut n.left, key),
-                Greater => Node::find_mut_rec(&mut n.right, key),
-                Equal   => Some(&mut n.value),
-            }
-        } else {None}
-    }
-
-    fn find_iter<'a>(mut ptr: &'a NodePtr<K, V>, key: &K) -> Option<&'a V> {
-        while let Some(ref n) = *ptr {
+    fn find_iter<'a, 'b>(mut ptr: &'a NodePtr<K, V>, key: &'b K) -> Option<&'a V> {
+        while let &Some(ref n) = ptr {
             match key.cmp(&n.key) {
                 Less    => { ptr = &n.left; }
                 Greater => { ptr = &n.right; }
@@ -72,4 +61,24 @@ impl<K: Ord, V> Node<K, V> {
 
         None
     }
+
+    fn find_mut<'a, 'b>(ptr: &'a mut NodePtr<K, V>, key: &'b K) -> Option<&'a mut V> {
+        if let &mut Some(ref mut n) = ptr {
+            match key.cmp(&n.key) {
+                Less    => Node::find_mut(&mut n.left, key),
+                Greater => Node::find_mut(&mut n.right, key),
+                Equal   => Some(&mut n.value),
+            }
+        } else {None}
+    }
+}
+
+//////////
+struct StringPair {
+    x : String,
+    y : String,
+}
+
+fn snd_len(&StringPair {x: ref sx, y: ref sy}: &StringPair) -> usize {
+    sx.len() + sy.len()
 }
