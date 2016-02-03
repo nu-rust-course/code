3a18d07f699ba3d8210dd3733a9ed22182bf5303
Jesse A. Tov <jesse.tov@gmail.com>
2016-02-03 12:20:23 -0600

    Iterative mutable tree lookup works now. Need to simplify.

--- ownership/src/bst.rs
+++ ownership/src/bst.rs
@@ -1,4 +1,6 @@
+use std::cmp::Ordering;
 use std::cmp::Ordering::*;
+use std::mem;
 
 pub struct BST<K, V>(NodePtr<K, V>);
 
@@ -10,6 +12,8 @@ struct Node<K, V> {
 }
 
 type NodePtr<K, V> = Option<Box<Node<K, V>>>;
+
+struct CursorMut<'a, K: 'a, V: 'a>(Option<&'a mut Node<K, V>>);
 
 impl<K, V> BST<K, V> {
     pub fn new() -> Self {
@@ -23,11 +27,11 @@ impl<K, V> BST<K, V> {
 
 impl<K: Ord, V> BST<K, V> {
     pub fn find(&self, key: &K) -> Option<&V> {
-        Node::find(&self.0, key)
+        Node::find_iter(&self.0, key)
     }
 
     pub fn find_mut(&mut self, key: &K) -> Option<&mut V> {
-        Node::find_mut(&mut self.0, key)
+        Node::find_mut_rec(&mut self.0, key)
     }
 }
 
@@ -40,17 +44,19 @@ impl<K, V> Node<K, V> {
 }
 
 impl<K: Ord, V> Node<K, V> {
-    fn find<'a, 'b>(ptr: &'a NodePtr<K, V>, key: &'b K) -> Option<&'a V> {
+    fn find_rec<'a, 'b>(ptr: &'a NodePtr<K, V>, key: &'b K) -> Option<&'a V> {
         if let &Some(ref n) = ptr {
             match key.cmp(&n.key) {
-                Less    => Node::find(&n.left, key),
-                Greater => Node::find(&n.right, key),
+                Less    => Node::find_rec(&n.left, key),
+                Greater => Node::find_rec(&n.right, key),
                 Equal   => Some(&n.value),
             }
         } else {None}
     }
 
-    fn find_iter<'a, 'b>(mut ptr: &'a NodePtr<K, V>, key: &'b K) -> Option<&'a V> {
+    fn find_iter<'a, 'b>(mut ptr: &'a NodePtr<K, V>, key: &'b K)
+        -> Option<&'a V>
+    {
         while let &Some(ref n) = ptr {
             match key.cmp(&n.key) {
                 Less    => { ptr = &n.left; }
@@ -62,14 +68,54 @@ impl<K: Ord, V> Node<K, V> {
         None
     }
 
-    fn find_mut<'a, 'b>(ptr: &'a mut NodePtr<K, V>, key: &'b K) -> Option<&'a mut V> {
+    fn find_mut_rec<'a, 'b>(ptr: &'a mut NodePtr<K, V>, key: &'b K)
+        -> Option<&'a mut V>
+    {
         if let &mut Some(ref mut n) = ptr {
             match key.cmp(&n.key) {
-                Less    => Node::find_mut(&mut n.left, key),
-                Greater => Node::find_mut(&mut n.right, key),
+                Less    => Node::find_mut_rec(&mut n.left, key),
+                Greater => Node::find_mut_rec(&mut n.right, key),
                 Equal   => Some(&mut n.value),
             }
         } else {None}
+    }
+
+    fn find_mut_iter<'a, 'b>(ptr: &'a mut NodePtr<K, V>, key: &'b K)
+        -> Option<&'a mut V>
+    {
+        let mut cur: CursorMut<'a, K, V> =
+            CursorMut(ptr.as_mut().map(|node| &mut **node));
+
+        loop {
+            match cur.key().map(|k| key.cmp(k)) {
+                Some(Less)     => { cur.left(); }
+                Some(Greater)  => { cur.right(); }
+                Some(Equal)    => { return cur.into_value(); }
+                None           => { return None; }
+            }
+        }
+    }
+}
+
+impl<'a, K, V> CursorMut<'a, K, V> {
+    fn left(&mut self) {
+        self.0.take().map(|node| {
+            self.0 = node.left.as_mut().map(|node| &mut **node);
+        });
+    }
+
+    fn right(&mut self) {
+        self.0.take().map(|node| {
+            self.0 = node.right.as_mut().map(|node| &mut **node);
+        });
+    }
+
+    fn key(&mut self) -> Option<&K> {
+        self.0.as_ref().map(|n| &n.key)
+    }
+
+    fn into_value(self) -> Option<&'a mut V> {
+        self.0.map(|n| &mut n.value)
     }
 }
 
