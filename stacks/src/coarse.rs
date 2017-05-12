e831638af074e68b0de7efc0322e16ba172c48df
Jesse A. Tov <jesse.tov@gmail.com>
2017-05-12 09:25:28 -0500

    Coarse-grained stack.

---
+++ stacks/src/coarse.rs
@@ -0,0 +1,95 @@
+//! Coarsely-synchronized stacks.
+
+use super::sequential as seq;
+
+use std::sync::{Mutex, MutexGuard};
+
+/// A coarsely-synchronized stack.
+///
+/// This can be shared between threads by wrapping it in an `Arc`.
+#[derive(Debug)]
+pub struct Stack<T>(Mutex<seq::Stack<T>>);
+
+impl<T> Stack<T> {
+    /// Returns a new, empty stack.
+    pub fn new() -> Self {
+        Self::from_seq(seq::Stack::new())
+    }
+
+    /// Converts a [sequential stack](../sequential/struct.Stack.html)
+    /// into a concurrent `Stack`.
+    pub fn from_seq(seq: seq::Stack<T>) -> Self {
+        Stack(Mutex::new(seq))
+    }
+
+    /// Converts a concurrent `Stack` into a [sequential
+    /// stack](../sequential/struct.Stack.html).
+    pub fn into_seq(self) -> seq::Stack<T> {
+        self.0.into_inner().expect("Stack mutex poisoned")
+    }
+
+    fn lock(&self) -> MutexGuard<seq::Stack<T>> {
+        self.0.lock().expect("Stack mutex poisoned")
+    }
+
+    /// Checks whether the stack is empty.
+    pub fn is_empty(&self) -> bool {
+        self.lock().is_empty()
+    }
+
+    /// Returns the number of elements in the stack.
+    pub fn len(&self) -> usize {
+        self.lock().len()
+    }
+
+    /// Pushes an element on top of the stack.
+    pub fn push(&self, data: T) {
+        self.lock().push(data)
+    }
+
+    /// Removes and returns the top element of the stack, or `None` if
+    /// empty.
+    pub fn try_pop(&self) -> Option<T> {
+        self.lock().try_pop()
+    }
+}
+
+impl<T: Clone> Clone for Stack<T> {
+    fn clone(&self) -> Self {
+        Stack::from_seq(self.lock().clone())
+    }
+}
+
+#[test]
+fn two_threads_cooperate() {
+    use std::{sync, thread};
+
+    let stack  = sync::Arc::new(Stack::new());
+    let stack1 = stack.clone();
+    let stack2 = stack.clone();
+
+    let handle2 = thread::spawn(move || {
+        for i in 5 .. 10 {
+            stack2.push(i);
+        }
+    });
+
+    let handle1 = thread::spawn(move || {
+        for i in 0 .. 5 {
+            stack1.push(i);
+        }
+    });
+
+    handle1.join().unwrap();
+    handle2.join().unwrap();
+
+    let mut actual = Vec::new();
+    while let Some(element) = stack.try_pop() {
+        actual.push(element);
+    }
+    actual.sort();
+
+    let expected: Vec<usize> = (0 .. 10).collect();
+
+    assert_eq!(expected, actual);
+}
--- stacks/src/sequential.rs
+++ stacks/src/sequential.rs
@@ -1,4 +1,8 @@
+//! Sequential stacks.
+
 /// A sequential stack.
+///
+/// Represented as a linked list, so all operations are O(1).
 ///
 /// # Example
 ///
@@ -15,13 +19,13 @@
 /// assert_eq!(Some(3), stack.try_pop());
 /// assert_eq!(None, stack.try_pop());
 /// ```
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 pub struct Stack<T> {
     head: Option<Box<Node<T>>>,
     len:  usize,
 }
 
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 struct Node<T> {
     data: T,
     next: Option<Box<Node<T>>>,
@@ -50,6 +54,26 @@ impl<T> Stack<T> {
     /// ```
     pub fn is_empty(&self) -> bool {
         self.head.is_none()
+    }
+
+    /// Returns the number of elements in the stack.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use stacks::sequential::Stack;
+    /// let mut stack = Stack::new();
+    ///
+    /// assert_eq!(0, stack.len());
+    /// stack.push(1);
+    /// assert_eq!(1, stack.len());
+    /// stack.push(2);
+    /// assert_eq!(2, stack.len());
+    /// stack.push(3);
+    /// assert_eq!(3, stack.len());
+    /// ```
+    pub fn len(&self) -> usize {
+        self.len
     }
 
     /// Pushes an element on top of the stack.
