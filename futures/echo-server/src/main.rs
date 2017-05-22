539c5ae9d5f6482335839e1b4bee42aea7bcd3a2
Jesse A. Tov <jesse.tov@gmail.com>
2017-05-22 17:06:59 -0500

    Added Echo Server example

---
+++ futures/echo-server/src/main.rs
@@ -0,0 +1,99 @@
+extern crate bytes;
+extern crate futures;
+extern crate tokio_io;
+extern crate tokio_proto;
+extern crate tokio_service;
+
+use std::io;
+use std::str;
+
+use bytes::BytesMut;
+
+use futures::{future, Future, BoxFuture};
+
+use tokio_io::codec::{Encoder, Decoder};
+use tokio_io::{AsyncRead, AsyncWrite};
+use tokio_io::codec::Framed;
+use tokio_proto::pipeline::ServerProto;
+use tokio_proto::TcpServer;
+use tokio_service::Service;
+
+pub struct LineCodec;
+
+impl Decoder for LineCodec {
+    type Item = String;
+    type Error = io::Error;
+
+    fn decode(&mut self, buf: &mut BytesMut) -> io::Result<Option<String>> {
+        if let Some(i) = buf.iter().position(|&b| b == b'\n') {
+            let line = buf.split_to(i); // Get up to the newline
+            buf.split_to(1);            // Discard the newline
+
+            if let Ok(s) = str::from_utf8(&line) {
+                Ok(Some(s.to_string()))
+            } else {
+                Err(io::Error::new(io::ErrorKind::Other, "invalid UTF-8"))
+            }
+        } else {
+            Ok(None)
+        }
+    }
+}
+
+impl Encoder for LineCodec {
+    type Item = String;
+    type Error = io::Error;
+
+    fn encode(&mut self, msg: String, buf: &mut BytesMut) -> io::Result<()> {
+        buf.extend(msg.as_bytes());
+        buf.extend(b"\n");
+        Ok(())
+    }
+}
+
+pub struct LineProto;
+
+impl<T: AsyncRead + AsyncWrite + 'static> ServerProto<T> for LineProto {
+    type Request = String;  // matches <LineCodec as Decoder>::Item
+    type Response = String; // matches <LineCodec as Encoder>::Item
+
+    type Transport = Framed<T, LineCodec>;
+    type BindTransport = Result<Self::Transport, io::Error>;
+
+    fn bind_transport(&self, io: T) -> Self::BindTransport {
+        Ok(io.framed(LineCodec))
+    }
+}
+
+pub struct EchoService;
+
+impl Service for EchoService {
+    type Request = String;
+    type Response = String;
+    type Error = io::Error;
+    type Future = BoxFuture<Self::Response, Self::Error>;
+
+    fn call(&self, req: Self::Request) -> Self::Future {
+        future::ok(req).boxed()
+    }
+}
+
+pub struct EchoRevService;
+
+impl Service for EchoRevService {
+    type Request = String;
+    type Response = String;
+    type Error = io::Error;
+    type Future = BoxFuture<Self::Response, Self::Error>;
+
+    fn call(&self, req: Self::Request) -> Self::Future {
+        let rev: String = req.chars().rev().collect();
+        future::ok(rev).boxed()
+    }
+}
+
+fn main() {
+    let server = TcpServer::new(LineProto, "0.0.0.0:12345".parse().unwrap());
+
+    server.serve(|| Ok(EchoRevService));
+}
