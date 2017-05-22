48e7a9bcceb5145cb8ba10cc5cacff06794df779
Jesse A. Tov <jesse.tov@gmail.com>
2017-05-22 17:18:18 -0500

    Timeout example.

--- futures/prime-timeout/src/main.rs
+++ futures/prime-timeout/src/main.rs
@@ -1,3 +1,50 @@
+extern crate futures;
+extern crate futures_cpupool;
+extern crate tokio_timer;
+
+use std::time::Duration;
+
+use futures::Future;
+use futures_cpupool::CpuPool;
+use tokio_timer::Timer;
+
+const BIG_PRIME: u64 = 15485867;
+const TIMEOUT: u64 = 750;
+
 fn main() {
-    println!("Hello, world!");
+    let pool = CpuPool::new_num_cpus();
+
+    let prime = pool.spawn_fn(|| {
+        Ok::<bool, ()>(is_prime(BIG_PRIME))
+    });
+
+    println!("Created the future");
+
+//    if prime_future.wait().unwrap() {
+//        println!("Prime");
+//    } else {
+//        println!("Not prime");
+//    }
+
+    let timeout = Timer::default()
+        .sleep(Duration::from_millis(TIMEOUT))
+        .then(|_| Err(()));
+
+    let winner = timeout.select(prime).map(|(win, _)| win);
+
+    match winner.wait() {
+        Ok(true) => println!("Prime"),
+        Ok(false) => println!("Not prime"),
+        Err(_) => println!("Timed out"),
+    }
 }
+
+/// Checks whether the given number is prime.
+fn is_prime(num: u64) -> bool {
+    for i in 2..num {
+        if num % i == 0 { return false }
+    }
+
+    true
+}
+
