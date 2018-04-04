bf0a17c0c1ae0296ce690ec8db589a32cb9fcd54
Jesse A. Tov <jesse.tov@gmail.com>
2018-04-04 16:20:47 -0500

    Undefined behavior examples.

---
+++ ub/src/bin/null_check.rs
@@ -0,0 +1,18 @@
+fn zero_ptr(p: &mut i32) {
+    *p = 0;
+}
+
+fn zero_optional_ptr(op: Option<&mut i32>)
+{
+    if let Some(p) = op {
+        *p = 0;
+    }
+}
+
+fn main() {
+    let mut x: i32 = 8;
+
+    zero_ptr(&mut x);
+    zero_optional_ptr(Some(&mut x));
+    zero_optional_ptr(None);
+}
