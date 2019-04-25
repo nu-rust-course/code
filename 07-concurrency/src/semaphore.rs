54bf2cf1b83b4e23d6f53a32c2baf535adfb2276
Jesse A. Tov <jesse.tov@gmail.com>
2019-04-25 10:55:17 -0500

    Redacted semaphore implementation for lecture starter.

--- 07-concurrency/src/semaphore.rs
+++ 07-concurrency/src/semaphore.rs
@@ -1,8 +1,9 @@
 //! Classic semaphores.
 
+#![allow(unused)]
 #![allow(clippy::mutex_atomic)]
 
-use std::default::Default;
+//use std::default::Default;
 use std::sync::{Mutex, Condvar};
 
 /// A semaphore is a synchronization primitive that keeps a count, which
@@ -14,83 +15,9 @@ pub struct Semaphore {
     condvar: Condvar,
 }
 
-/// An RIAA guard that automatically raises a semaphore when dropped.
-/// Created by [`Semaphore::access`](fn.access.html).
-#[derive(Debug)]
-pub struct SemaphoreGuard<'a>(&'a Semaphore);
-
-impl<'a> Drop for SemaphoreGuard<'a> {
-    fn drop(&mut self) {
-        self.0.raise()
-    }
-}
-
-impl Semaphore {
-    /// Creates a new semaphore with the given initial count.
-    pub fn new(count: usize) -> Self
-    {
-        Semaphore {
-            counter: Mutex::new(count),
-            condvar: Condvar::new(),
-        }
-    }
-
-    /// Returns the current count of the semaphore. Note that the
-    /// semaphore count may change between a call to `count` and any
-    /// subsequent attempts to raise or lower.
-    pub fn count(&self) -> usize {
-        *self.counter.lock().unwrap()
-    }
-
-    /// Raises the semaphore. This operation always succeeds.
-    pub fn raise(&self) {
-        *self.counter.lock().unwrap() += 1;
-        self.condvar.notify_one();
-    }
-
-    /// Lowers the semaphore; if the count is 0, blocks until another
-    /// thread raises.
-    pub fn lower(&self) {
-        let mut guard = self.counter.lock().unwrap();
-
-        while *guard == 0 {
-            guard = self.condvar.wait(guard).unwrap();
-        }
-
-        *guard -= 1;
-    }
-
-    /// Tries to lower the semaphore without blocking; returns `true`
-    /// for success or `false` if the count was 0.
-    pub fn try_lower(&self) -> bool {
-        let mut guard = self.counter.lock().unwrap();
-
-        if *guard == 0 {
-            false
-        } else {
-            *guard -= 1;
-            true
-        }
-    }
-
-    /// Lowers the semaphore and returns an RAII guard that will
-    /// automatically raise it when dropped.
-    pub fn access(&self) -> SemaphoreGuard {
-        self.lower();
-        SemaphoreGuard(self)
-    }
-}
-
-impl Default for Semaphore {
-    /// The default semaphore starts with a count of 0.
-    fn default() -> Self {
-        Semaphore::new(0)
-    }
-}
-
 // These tests don't test much. I'm not sure how to test concurrency
 // primitives.
-#[cfg(test)]
+#[cfg(all(test, ignore_please))]
 mod test {
     use super::Semaphore;
 
